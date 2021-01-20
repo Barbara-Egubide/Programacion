9dbcc09ba0eaf50edf1869f7fad893e4c4ae06aa
Barbara López <73885464+Barbara-Egubide@users.noreply.github.com>
2021-01-20 18:09:20 +0100

    Add files via upload

---
+++ Tema7/Practica1/Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,12 @@
+package ejercicio1;
+
+import Vista.*;
+
+public class Ejercicio1 {
+
+    private static Aleatorio v1;
+    public static void main(String[] args) {
+       v1 = new Aleatorio();
+       v1.setVisible(true);
+    }
+}
